0b207e7dc118ace8dbcfae150d9c2e88201dfe5a
Redowan0019 <146367158+Redowan0019@users.noreply.github.com>
2024-03-27 12:24:43 +0600

    Advanced Combat and parry....Database

--- src/main/java/com/example/return_3/main/Game.java
+++ src/main/java/com/example/return_3/main/Game.java
@@ -131,9 +131,9 @@ public final int messageState=10;
     public void start(Stage stage) throws Exception {
 
         primaryStage = stage; //as we have a static Stage variable . we initialize the value as game stage of start method
-        gameInstance= new Game(); // we create a
-        showGameScene(); // now we are redirecting the showGameScene method
-       // loginPage();
+        gameInstance= this;  // we create a
+        //showGameScene(); // now we are redirecting the showGameScene method
+        loginPage();
 //        loadMenuScene();
         primaryStage.setTitle("Powered By return_3;"); //set the title of the stage
         primaryStage.initStyle(StageStyle.UNDECORATED); //create un decorated style
