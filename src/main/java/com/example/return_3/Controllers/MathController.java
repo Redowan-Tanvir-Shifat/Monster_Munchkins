3a110e3fa5e4898e5e578756cb3b74f32f4f2130
Redowan0019 <146367158+Redowan0019@users.noreply.github.com>
2024-03-29 16:52:29 +0600

    New school and Sign up page...

--- src/main/java/com/example/return_3/Controllers/MathController.java
+++ src/main/java/com/example/return_3/Controllers/MathController.java
@@ -10,23 +10,14 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 
 public class MathController {
-    public Button addBtn;
-    public Button multiplicationBtn;
-    public Button exponentsBtn;
-    public Button decimalBtn;
     public Button algebraBtn;
-    public Button numbersBtn;
     public Button geometryBtn;
-    public Button geometricBtn;
     public Button conversionBtn;
-    public Button percentagesBtn;
-    public Button absoluteBtn;
-    public Button setsbtn;
-    public Button divisibilityBtn;
+    public Button triginometryBtn;
+    public Button backButton;
     Game game;
 
-    @FXML
-    private Button backButton;
+  
 
     @FXML
     void back(ActionEvent event) throws Exception {
@@ -36,43 +27,16 @@ public class MathController {
         //scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("school.css")).toExternalForm());
         Game.primaryStage.setScene(scene);
     }
-
-    public void goToAdd_sub(ActionEvent event) {
-    }
-
-    public void goToMultiplication(ActionEvent event) {
-    }
-
-    public void goToExponents(ActionEvent event) {
-    }
-
-    public void goToDecimals(ActionEvent event) {
-    }
-
+    
     public void goToAlgebra(ActionEvent event) {
     }
-
-    public void goToNumbers(ActionEvent event) {
-    }
-
+    
     public void goToGeometry(ActionEvent event) {
     }
-
-    public void goToGeometric(ActionEvent event) {
-    }
-
+    
     public void goToConversion(ActionEvent event) {
     }
-
-    public void goToPercentages(ActionEvent event) {
-    }
-
-    public void goToAbsolute(ActionEvent event) {
-    }
-
-    public void goToSets(ActionEvent event) {
-    }
-
-    public void goTodivisibility(ActionEvent event) {
+    
+    public void goToTrigonometry(ActionEvent event) {
     }
 }
